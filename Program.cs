42fce00760cfdd61ac8821588939b62ffd7b72ac
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-11 12:49:37 -0800

    add punctuations to quotes

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace myApp
         Console.WriteLine("The company you keep has a large influence on the situations you bring to yourself. Birds of a feather flock together.");
       } else if (intUserNumber == 9)
       {
-        Console.WriteLine("The most important thing, number one, is you gotta get rid of doubt. If you got doubt in what you're doing, it's not gonna work and the way to do that is to have a plan. 'Cause if you got a plan, it's not just like a pipe-dream, you have a step-by-step list of things to do to get to your goal. If you don't have that, it's very hard to really have faith in what you're doing 'cause soon as something pop up, it's gonna look like the end-all");
+        Console.WriteLine("The most important thing, number one, is you gotta get rid of doubt. If you got doubt in what you're doing, it's not gonna work and the way to do that is to have a plan. 'Cause if you got a plan, it's not just like a pipe-dream, you have a step-by-step list of things to do to get to your goal. If you don't have that, it's very hard to really have faith in what you're doing 'cause soon as something pop up, it's gonna look like the end-all.");
       }
       else if (intUserNumber == 8)
       {
@@ -24,15 +24,15 @@ namespace myApp
       }
       else if (intUserNumber == 7)
       {
-        Console.WriteLine("Longwinded, running through this life like it was mine/ Never settling, but setting every goal high/ One thousand burpees on the path to my own destruction or success/ But what is a mistake without the lesson?/ See, the best teacher in life is your own experience/ None of us know who we are until we fail/ They say every man is defined by his reaction to any given situation/ Well who would you want to define you?/ Someone else or yourself? Whatever you do, homie, give your heart to it/ And stay strong");
+        Console.WriteLine("Longwinded, running through this life like it was mine. Never settling, but setting every goal high. One thousand burpees on the path to my own destruction or success. But what is a mistake without the lesson? See, the best teacher in life is your own experience. None of us know who we are until we fail. They say every man is defined by his reaction to any given situation. Well who would you want to define you? Someone else or yourself? Whatever you do, homie, give your heart to it and stay strong.");
       }
       else if (intUserNumber == 6)
       {
-        Console.WriteLine("The best thing you can do for a person is to inspire them. That's the best currency you can offer: inspiration. So, when a person can rely on you for that, that empowers them in every realm of their life. Being inspired. It empowers them in their relationships, in their business, in their art, in their creativity. It empowers them because without inspiration, you're dry");
+        Console.WriteLine("The best thing you can do for a person is to inspire them. That's the best currency you can offer: inspiration. So, when a person can rely on you for that, that empowers them in every realm of their life. Being inspired. It empowers them in their relationships, in their business, in their art, and in their creativity. It empowers them because without (in)spiration, you're empty.");
       }
       else if (intUserNumber == 5)
       {
-        Console.WriteLine("Define who you are and what you are, and be clear on that. Meditate on that and then, live and die by that... You can't break the rules, the fundamental rules. Be a man of your word, do what you say you're gonna do, respect people the way you would like to be respected.");
+        Console.WriteLine("Define who you are and what you are, and be clear on that. Meditate on that and then, live and die by that. You can't break the rules, the fundamental rules. Be a man of your word, do what you say you're gonna do, respect people the way you would like to be respected.");
       }
       else if (intUserNumber == 4)
       {
@@ -44,7 +44,11 @@ namespace myApp
       }
       else if (intUserNumber == 2)
       {
-        Console.WriteLine("I’m about seeing long-term, seeing a vision, understanding nothing really worthwhile happens overnight, and just sticking to your script long enough to make something real happen");
+        Console.WriteLine("I’m about seeing long-term, seeing a vision, understanding nothing really worthwhile happens overnight, and just sticking to your script long enough to make something real happen.");
+      }
+      else if (intUserNumber == 1)
+      {
+        Console.WriteLine("It's possible to monetize your art without compromising the integrity of it for commerce.");
       }
       else if (intUserNumber == 1)
       {
